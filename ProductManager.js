4c8c7db7f7aea6a29b421520b3052e1f2c9b95df
Ignacio Alvarez <alvarezignacio66@gmail.com>
2024-02-21 20:43:38 -0300

    entrega servidor express

--- ProductManager.js
+++ ProductManager.js
@@ -17,17 +17,20 @@ class ProductManager {
         }
     }
 
-    async getProducts() {
+    getProducts() {
         try {
-            return await this.getProductsFromFile();
+            const data = fs.readFileSync(this.path, 'utf8');
+            return JSON.parse(data);
         } catch (error) {
-            throw new Error("Error encontrando producto");
+            console.log('Error al cargar productos desde archivo:', error.message);
+            return [];
         }
     }
 
+
     async getProductById(id) {
         try {
-            const products = await this.getProductsFromFile();
+            const products = await this.getProducts();
             return products.find(product => product.id === id);
         } catch (error) {
             throw new Error("Error encontrando producto por id");
@@ -39,7 +42,7 @@ class ProductManager {
             const products = await this.getProductsFromFile();
             const index = products.findIndex(product => product.id === id);
             if (index !== -1) {
-                products[index] = { ...products[index], ...actualizarProductos};
+                products[index] = { ...products[index], ...actualizarProductos };
                 await this.saveProducts(products);
             } else {
                 throw new Error("Producto no encontrado con id: " + id);
@@ -59,20 +62,21 @@ class ProductManager {
         }
     }
 
-    async getProductsFromFile() {
+    async getProductsFromFile(path) {
         return new Promise((resolve, reject) => {
-            fs.readFile(this.path,  (e, data) => {
-                if (e) {
-                    if (e.codigo === "ERROR!") {
+            // Leer el archivo
+            fs.readFile(path, (error, data) => {
+                if (error) {
+                    if (error.code === 'ENOENT') {
                         resolve([]);
                     } else {
-                        reject(e);
+                        reject(error);
                     }
                 } else {
                     try {
                         resolve(JSON.parse(data));
-                    } catch (error) {
-                        reject(new Error("Error!"));
+                    } catch (parseError) {
+                        reject(new Error('Error al analizar los datos JSON'));
                     }
                 }
             });
@@ -92,6 +96,8 @@ class ProductManager {
         });
     }
 }
+
+
 
 
 module.exports = ProductManager;
---
+++ src/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const ProductManager = require('../ProductManager.js');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const productManager = new ProductManager('products.json');
+
+app.get('/', (req, res) => {
+    res.send('Bienvenido a la tienda!!.');
+}
+);
+
+
+app.get('/products/:pid', async (req, res) => {
+    const productId = parseInt(req.params.pid);
+    const product = await productManager.getProductById(productId);
+    if (product) {
+        res.json(product);
+    } else {
+        res.status(404).json({ error: 'Producto no encontrado!' });
+    }
+});
+
+
+app.get('/products', (req, res) => {
+    let limit = req.query.limit;
+    let products = productManager.getProducts();
+
+    if (limit && !isNaN(limit)) {
+        limit = parseInt(limit);
+        products = products.slice(0, limit);
+    }
+
+    res.json(products);
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
